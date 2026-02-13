be856516be2a9fa5654efa0b1108c8711e60713f
Gothelph <gothelph.dourden@gmail.com>
2026-02-13 22:37:53 +0300

    9

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       {/* контент поверх картинки */}
       <div className="relative z-10 flex flex-col items-center space-y-4 px-6">
         <img
-          src="../logo.png"
+          src="../../public/logo.png"
           alt="logo"
           className="w-32 h-32 sm:w-40 sm:h-40 rounded-full border-4 border-white shadow-lg"
         />
